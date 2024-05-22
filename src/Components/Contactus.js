3a2d207668ce3c11b34918458eaf3b1acd006bdd
manicdon7 <manicdon7@gmail.com>
2024-05-22 20:50:05 +0530

    contactus

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -1,42 +1,55 @@
 import React from 'react'
-import contactimg from '../assets/contactusimg.png';
+import servicepng from '../assets/service.png';
+import mail from '../assets/mail.png';
+import phone from '../assets/phone.png';
+import peacockimg from '../assets/peacockwatermark-2 1.png';
 
 const Contactus = () => {
     return (
         <div>
+            <div className='flex justify-center mt-3'>
+                <div className='bg-cover bg-center w-screen' style={{ backgroundImage: `url(${servicepng})` }}>
+                    <div className='text-white text-2xl md:text-7xl font-bold py-24 md:py-40 text-center'>
+                        <p className='border-2 border-white mx-4 md:mx-40 py-10 md:py-8 rounded-2xl'>Contact Us</p>
+                    </div>
+                </div>
+            </div>
+            <div className='grid grid-cols-2'>
             <div className='mx-10 md:mx-32 my-20'>
                 <div>
-                    <p className='text-4xl text-[#F1518B]'>
-                        CONTACT US
-                    </p>
+                    <p className='text-[#FD8901] font-bold text-6xl'>Visit us</p>
                 </div>
-                <div className='grid md:grid-cols-2 grid-cols-1'>
-                    <div className=''>
-                        <img src={contactimg} alt='contactimg' className='py-10' />
-                    </div>
-                    <div className='py-10'>
-                        <div>
-                            <p className='text-[#F1518B] font-semibold text-3xl'>Visit us</p>
-                        </div>
-                        <p className='font-normal text-lg py-6'>
-                            No:7, 545, Velachery Main Rd, United Colony,
-                            Vijayanagaram, Ranganathapuram, Medavakkam,
-                            Chennai, Tamil Nadu 600100
-                        </p>
-                        <div>
-                            <p className='text-[#F1518B] font-semibold text-3xl'>Contact</p>
-                            <p className='font-normal text-lg pt-3 py-1'>contact@company.com</p>
-                            <p className='font-normal text-lg py-1'>+91 99402 29923</p>
-                            <div className='space-y-5'>
-                                <input placeholder='   Email' className='w-80 md:w-96 py-2 rounded-full border-2 hover:border-[#F1518B] bg-[#F2F2F2]' /> <br />
-                                <textarea placeholder='   Message' className='py-3 w-80 md:w-96 rounded-full border-2 hover:border-[#F1518B] bg-[#F2F2F2]' /> <br/>
-                                <button className='border border-[#F1518B] hover:bg-[#F1518B] hover:text-white px-10 py-2 rounded-3xl font-semibold'>Submit</button>
-                            </div>
+                <p className='font-medium text-2xl py-6'>
+                    No:11/163, Vadakkupattu mainroad, <br />
+                    Kovilambakkam, Chennai-600129
 
+                </p>
+                <div>
+                    <p className='text-[#FD8901] font-bold text-6xl my-5'>Contact</p>
+                    <div>
+                        <div className='flex space-x-7'>
+                            <img src={mail} alt='mail' className='h-7 w-7 my-3' />
+                            <p className='font-medium text-2xl pt-3 py-1'>rajadecorator1980@gmail.com</p>
+                        </div>
+                        <div className='flex space-x-7'>
+                            <img src={phone} alt='phone' className='h-7 w-7 my-1' />
+                            <p className='font-medium text-2xl py-1'>+91 99402 29923, +91 98406 01980</p>
                         </div>
                     </div>
                 </div>
             </div>
+            <div>
+                <img src={peacockimg} alt='peacock' className='mt-32 mx-20'/>
+            </div>
+            </div>
+            <div className='space-y-5 mx-32 mb-20'>
+                        <h1 className='font-bold text-6xl text-[#FD8901]'>Email us</h1>
+                        <input placeholder='   name' type='email' className='w-80 md:w-6/12 py-3 rounded-lg border-2 hover:border-[#FD8901] bg-[#F2F2F2]' /> <br />
+                        <input placeholder='   phone' type='number' className='w-80 md:w-6/12 py-3 rounded-lg border-2 hover:border-[#FD8901] bg-[#F2F2F2]' /> <br />
+                        <input placeholder='   Email' type='text' className='w-80 md:w-6/12 py-3 rounded-lg border-2 hover:border-[#FD8901] bg-[#F2F2F2]' /> <br />
+                        <textarea placeholder='   Message' className='py-3 w-80 md:w-6/12 rounded-lg border-2 hover:border-[#FD8901] bg-[#F2F2F2]' /> <br />
+                        <button className='py-3 w-80 md:w-6/12 rounded-lg border-2 bg-[#FD8901] text-white hover:border-[#FD8901] bg-[#F2F2F2]font-semibold'>Send Message</button>
+                    </div>
         </div>
     )
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img from '../assets/WhatsApp Image 2024-05-08 at 11.44.10_e34a28c8.jpg'
 import hall from '../assets/hall.png';
+import peacock from '../assets/peacock.png';
 
 const About = () => {
   return (
@@ -18,6 +19,12 @@ const About = () => {
                 <img src={hall} alt='hall' className='w-10/12 h-96 border-8 rounded-3xl border-yellow-500 '/>
             </div>
             </div>
+            <div>
+                <p className='mx-32 text-2xl font-normal my-10'>At Raja Decors, we believe that flowers have the power to transform ordinary moments into extraordinary experiences. Our commitment to "Crafting Unforgettable Moments with Floral Excellence" is more than just a tagline—it's the cornerstone of everything we do.</p>
+            </div>
+        <div className='flex justify-end mx-20'>
+            <img src={peacock} alt="logo" className='h-60 w-60 md:block hidden' />
+        </div>
         </div>
     </div>
   )
